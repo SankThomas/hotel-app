38ba70081f12e438b259c1f61867b9193452868a
Thomas Sankara <thomassankara74@gmail.com>
2021-02-26 23:14:08 +0300

    rooms data array

---
+++ src/data/rooms.js
@@ -0,0 +1,70 @@
+import { v4 as uuidv4 } from "uuid"
+
+export const rooms = [
+  {
+    id: uuidv4(),
+    title: "Room 1",
+    image: "./images/room-1.jpg",
+    desc: "Presidential",
+  },
+  {
+    id: uuidv4(),
+    title: "Room 2",
+    image: "./images/room-2.jpg",
+    desc: "Presidential",
+  },
+  {
+    id: uuidv4(),
+    title: "Room 3",
+    image: "./images/room-3.jpg",
+    desc: "Presidential",
+  },
+  {
+    id: uuidv4(),
+    title: "Room 4",
+    image: "./images/room-4.jpg",
+    desc: "Double Deluxe",
+  },
+  {
+    id: uuidv4(),
+    title: "Room 5",
+    image: "./images/room-5.jpg",
+    desc: "Double Deluxe",
+  },
+  {
+    id: uuidv4(),
+    title: "Room 6",
+    image: "./images/room-6.jpg",
+    desc: "Double Deluxe",
+  },
+  {
+    id: uuidv4(),
+    title: "Room 7",
+    image: "./images/room-7.jpg",
+    desc: "Single Deluxe",
+  },
+  {
+    id: uuidv4(),
+    title: "Room 8",
+    image: "./images/room-8.jpg",
+    desc: "Single Deluxe",
+  },
+  {
+    id: uuidv4(),
+    title: "Room 9",
+    image: "./images/room-9.jpg",
+    desc: "Single Deluxe",
+  },
+  {
+    id: uuidv4(),
+    title: "Room 10",
+    image: "./images/room-10.jpg",
+    desc: "Bed & Breakfast",
+  },
+  {
+    id: uuidv4(),
+    title: "Room 11",
+    image: "./images/room-11.jpg",
+    desc: "Bed & Breakfast",
+  },
+]
