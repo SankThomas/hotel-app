1c7fd0a335bfa543b6d65a7cc035d9a308f5ec56
Thomas Sankara <thomassankara74@gmail.com>
2021-02-26 11:37:27 +0300

    Showcase

---
+++ src/components/Showcase.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+const Showcase = () => {
+  return (
+    <>
+      <section className="showcase">
+        <div className="overlay flex flex-col items-center justify-center">
+          <div className="text p-5">
+            <h1 className="text-primary text-4xl mb-2 lg:text-6xl">
+              Luxurious Rooms
+            </h1>
+            <div className="bg-primary h-1 w-20 mx-auto"></div>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Showcase
