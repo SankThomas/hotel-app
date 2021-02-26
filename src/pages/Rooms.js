c16c4f5247965e1c20f17d06a304f28033dce50e
Thomas Sankara <thomassankara74@gmail.com>
2021-02-26 23:14:34 +0300

    render and style rooms

--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,9 +1,29 @@
-import React from "react"
+import { useState } from "react"
+import RoomsShowcase from "../components/RoomsShowcase"
+import { rooms } from "../data/rooms"
 
 const Rooms = () => {
+  // eslint-disable-next-line
+  const [cards, setRooms] = useState(rooms)
+
   return (
     <>
-      <h1>Rooms</h1>
+      <RoomsShowcase />
+      <section className="bg-gray-100 pt-10 pb-20 px-5 grid grid-cols-1 gap-10 sm:grid-cols-2 sm:gap-5 lg:grid-cols-3 xl:px-32">
+        {cards.map((card) => {
+          const { id, title, image, desc } = card
+
+          return (
+            <article
+              key={id}
+              className="p-5 bg-white shadow-lg transition-all duration-300 hover:shadow-sm"
+            >
+              <img src={image} alt={title} />
+              <h4 className="pt-2">{desc}</h4>
+            </article>
+          )
+        })}
+      </section>
     </>
   )
 }
