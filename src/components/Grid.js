612487e50f1cdef28690f5cf24b6e235b5dd13b4
Thomas Sankara <thomassankara74@gmail.com>
2021-02-26 22:14:29 +0300

    add header, transition opacity and more styles

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -6,14 +6,17 @@ import grid3 from "../images/room-5.jpg"
 const Grid = () => {
   return (
     <>
-      <section className="grid grid-cols-1 gap-5 pt-5 pb-20 md:grid-cols-2 lg:grid-cols-3 lg:gap-3 xl:px-32">
-        <div>
+      <h3 className="text-center text-3xl font-bold mb-2 text-primary">
+        Featured Rooms
+      </h3>
+      <section className="grid grid-cols-1 gap-5 pt-5 px-5 pb-20 md:grid-cols-2 lg:grid-cols-3 lg:gap-3 xl:px-32">
+        <div className="hover:opacity-80 transition-opacity">
           <img src={grid1} alt="" className="sm:h-80 sm:w-full" />
         </div>
-        <div>
+        <div className="hover:opacity-80 transition-opacity">
           <img src={grid2} alt="" className="sm:h-80 sm:w-full" />
         </div>
-        <div>
+        <div className="hover:opacity-80 transition-opacity">
           <img src={grid3} alt="" className="sm:h-80 sm:w-full" />
         </div>
       </section>
