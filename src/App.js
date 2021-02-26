5cce42f849e4e94839214797d75846f0fe9d27da
Thomas Sankara <thomassankara74@gmail.com>
2021-02-26 11:38:29 +0300

    set up react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 
-function App() {
+// Components
+import Header from "./components/Header"
+import Showcase from "./components/Showcase"
+
+// Pages
+import Rooms from "./pages/Rooms"
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      {/* ScrollToTop */}
+      <Header />
+
+      <Switch>
+        <Route path="/" exact>
+          <Showcase />
+        </Route>
+        <Route path="/rooms">
+          <Rooms />
+        </Route>
+        <Route path="/rooms:id">{/* <Room /> */}</Route>
+      </Switch>
+    </Router>
+  )
 }
 
-export default App;
+export default App
