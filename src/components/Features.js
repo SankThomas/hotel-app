4f394e9f971985dc52b0dcee367db387b8fc4f8b
Thomas Sankara <thomassankara74@gmail.com>
2021-02-26 13:36:59 +0300

    style Features component

--- src/components/Features.js
+++ src/components/Features.js
@@ -6,19 +6,25 @@ const Features = () => {
     <>
       <section>
         <div>
-          <img src={restaurant} alt="" />
-          <h4>Restaurant, meeting & conference center</h4>
+          <img src={restaurant} alt="" className="block mx-auto lg:w-9/12" />
+          <h4 className="px-5 mt-2 mb-3 text-2xl text-primary lg:text-5xl xl:px-48">
+            Restaurant, meeting & conference center
+          </h4>
         </div>
-        <div>
+        <div className="px-5 pb-10 md:flex md:justify-end xl:px-32">
           <ul>
-            <li>Located in the outskirts of Nairobi city</li>
-            <li>8 bars and restaurants</li>
-            <li>600 acre land</li>
-            <li>202 beautiful and spacious rooms</li>
-            <li>10 penthouse suites</li>
-            <li>Rooftop pool</li>
-            <li>Indoor pools for select suites</li>
-            <li>Meeting & conference centers</li>
+            <li className="my-2 md:text-xl">
+              Located in the outskirts of Nairobi city
+            </li>
+            <li className="my-2 md:text-xl">8 bars and restaurants</li>
+            <li className="my-2 md:text-xl">600 acre land</li>
+            <li className="my-2 md:text-xl">
+              202 beautiful and spacious rooms
+            </li>
+            <li className="my-2 md:text-xl">10 penthouse suites</li>
+            <li className="my-2 md:text-xl">Rooftop pool</li>
+            <li className="my-2 md:text-xl">Indoor pools for select suites</li>
+            <li className="my-2 md:text-xl">Meeting & conference centers</li>
           </ul>
         </div>
       </section>
