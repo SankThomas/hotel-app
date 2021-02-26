4937b9726b513bc09a2153a178d474183878124f
Thomas Sankara <thomassankara74@gmail.com>
2021-02-26 23:13:44 +0300

    showcase for Rooms page

---
+++ src/components/RoomsShowcase.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+const RoomsShowcase = () => {
+  return (
+    <>
+      <section className="rooms-showcase">
+        <div className="overlay flex flex-col items-center justify-center">
+          <h3 className="px-4 py-3 text-4xl lg:text-6xl text-primary">
+            Browse Rooms
+            <div className="bg-primary h-1 w-20 mx-auto my-2"></div>
+          </h3>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default RoomsShowcase
