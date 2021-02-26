07794406ad8de0295a61b8b369f214daeb9af9ae
Thomas Sankara <thomassankara74@gmail.com>
2021-02-26 13:37:49 +0300

    offers data array

---
+++ src/data/offers.js
@@ -0,0 +1,25 @@
+import { v4 as uuidv4 } from "uuid"
+
+export const offers = [
+  {
+    id: uuidv4(),
+    title: "Candle-lit dinner",
+    desc:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit, rem! Eveniet iure tenetur minima velit. Iure iusto quisquam officia magni.",
+    image: "./images/offers-1.jpg",
+  },
+  {
+    id: uuidv4(),
+    title: "Open bar",
+    desc:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit, rem! Eveniet iure tenetur minima velit. Iure iusto quisquam officia magni.",
+    image: "./images/offers-2.jpg",
+  },
+  {
+    id: uuidv4(),
+    title: "Style points",
+    desc:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit, rem! Eveniet iure tenetur minima velit. Iure iusto quisquam officia magni.",
+    image: "./images/room-7.jpg",
+  },
+]
