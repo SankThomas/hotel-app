756a5f7dfe341a91fb586388f46a6170025388cc
Thomas Sankara <thomassankara74@gmail.com>
2021-02-26 11:37:10 +0300

    Book Now button

---
+++ src/components/BookNow.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+const BookNow = () => {
+  return (
+    <>
+      <div>
+        <button className="bg-primary text-white py-2 px-4 rounded-lg transition-all border-2 hover:bg-white hover:text-black">
+          Book Now
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default BookNow
