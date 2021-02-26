441d7d48adc502785e11c5c58b40fee29bfdc482
Thomas Sankara <thomassankara74@gmail.com>
2021-02-26 23:13:06 +0300

    increase image and paragraph widths

--- src/components/Comfy.js
+++ src/components/Comfy.js
@@ -6,17 +6,17 @@ const Comfy = () => {
     <>
       <section className="bg-red-900 py-20">
         <div>
-          <img src={comfy} alt="" className="block mx-auto lg:w-2/3" />
+          <img src={comfy} alt="" className="block mx-auto lg:w-9/12" />
         </div>
-        <div className="lg:flex lg:items-center lg:justify-between px-5 text-white mt-4 lg:w-2/3 lg:mx-auto lg:px-0">
+        <div className="lg:flex lg:items-center lg:justify-between px-5 text-white mt-4 lg:w-9/12 lg:mx-auto lg:px-0">
           <div>
             <h4>Wine in class</h4>
             <p>Bars are open from 6pm to 2am</p>
           </div>
           <div>
-            <p className="mt-5 lg:w-96 lg:mt-0">
+            <p className="mt-5 lg:w-80 lg:mt-0">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
-              ab unde dolorum adipisci expedita debitis.
+              ab unde dolorum adipisci expedita.
             </p>
           </div>
         </div>
