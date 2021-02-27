e6981a717093fd6a830f943f8d3e0dfbe1e895e5
Thomas Sankara <thomassankara74@gmail.com>
2021-02-27 09:01:43 +0300

    reduce title for small screens

--- src/components/RoomsShowcase.js
+++ src/components/RoomsShowcase.js
@@ -5,7 +5,7 @@ const RoomsShowcase = () => {
     <>
       <section className="rooms-showcase">
         <div className="overlay flex flex-col items-center justify-center">
-          <h3 className="px-4 py-3 text-4xl lg:text-6xl text-primary">
+          <h3 className="px-4 py-3 text-3xl md:text-4xl lg:text-6xl text-primary">
             Browse Rooms
             <div className="bg-primary h-1 w-20 mx-auto my-2"></div>
           </h3>
