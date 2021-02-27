ed0a85a39e8055a2321b83c9445d12e0bf33c133
Thomas Sankara <thomassankara74@gmail.com>
2021-02-27 09:02:18 +0300

    reduce font-size, add padding

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -4,13 +4,13 @@ const Showcase = () => {
   return (
     <>
       <section className="showcase">
-        <div className="overlay flex flex-col items-center justify-center">
+        <div className="overlay flex flex-col items-center justify-center px-5">
           <div className="text p-5">
-            <h1 className="text-primary text-4xl mb-2 lg:text-6xl">
+            <h1 className="text-primary text-center text-3xl mb-2 md:text-4xl lg:text-6xl">
               Luxurious Rooms
             </h1>
             <div className="bg-primary h-1 w-20 mx-auto"></div>
-            <p className="text-center text-xl mt-2 md:text-2xl">
+            <p className="text-center text-lg mt-2 md:text-2xl">
               Book a room in one of Kenya's top-rated hotels
             </p>
           </div>
