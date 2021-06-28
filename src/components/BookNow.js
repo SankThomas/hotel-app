6649ba1f61de8e5381d16e855a16485cbbdea61b
Thomas Sankara <thomassankara74@gmail.com>
2021-06-28 11:43:10 +0300

    BookNow button component

--- src/components/BookNow.js
+++ src/components/BookNow.js
@@ -1,12 +1,16 @@
 import React from "react"
+import { NavLink } from "react-router-dom"
 
 const BookNow = () => {
   return (
     <>
       <div>
-        <button className="bg-primary text-white py-2 px-4 rounded-lg transition-all border-2 hover:bg-white hover:text-black">
+        <NavLink
+          to="/book-now"
+          className="bg-primary text-white py-2 px-4 rounded-lg transition-all border-2 hover:bg-white hover:text-black"
+        >
           Book Now
-        </button>
+        </NavLink>
       </div>
     </>
   )
